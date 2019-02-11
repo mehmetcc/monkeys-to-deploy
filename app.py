0540fa1a837aeef5d6b2f28abf30093fcacb1ffa
mehmet <mehmetccm@gmail.com>
2019-02-11 21:36:06 +0300

    first

--- app.py
+++ app.py
@@ -80,6 +80,9 @@ def upload():
         # Make prediction
         preds = model_predict(file_path, model)
 
+        # Clean up
+        os.remove(file_path)
+
         # Process your result for human
         # pred_class = preds.argmax(axis=-1)            # Simple argmax
         # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
